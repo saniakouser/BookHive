18e70cd58723eedc781271064d5449e4de8de839
Mohd Saif <mohdsaif9990197398@gmail.com>
2025-06-14 01:06:25 +0530

    register login using auth context

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import router from './Path';
 import { RouterProvider } from 'react-router-dom';
+import { AuthProvider } from './Context/AuthContext';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-   <RouterProvider router={router} />
-  </React.StrictMode>
+  <AuthProvider>
+    <React.StrictMode>
+    <RouterProvider router={router} />
+    </React.StrictMode>
+  </AuthProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
